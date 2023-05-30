9dd6ea9026528768301357efb2f90254d1eda44b
pawanmukati <111895563+pawanmukati@users.noreply.github.com>
2023-05-30 21:17:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const env = require("dotenv");
 const cors = require("cors");
 const pdfRoute = require("./pdfRoutes");
 const morgan = require('morgan')
+const path = require('path')
 
 env.config();
 
@@ -12,7 +13,7 @@ const port = 8001;
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'))
-
+app.use('/static', express.static(path.join(__dirname, 'document')))
 app.use(pdfRoute)
 
 app.listen(port,()=>{
