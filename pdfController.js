4d5b99d58226934a8782c4fda7c7432c8869c7bf
pawanmukati <pawanmukati765@gmail.com>
2023-05-31 13:07:33 +0530

    31-05-2023-1

--- pdfController.js
+++ pdfController.js
@@ -2,6 +2,7 @@
 
 const pdf = require("html-pdf");
 const path = require("path");
+const puppeteer = require('puppeteer');
 const nodemailer = require("nodemailer");
 const fs = require("fs");
 const pdfTemplate = require("./document/document");
@@ -12,40 +13,30 @@ const puppeteer = require('puppeteer');
 env.config();
 
 exports.createPdf = (req, res) => {
-  // const options = {
-  //   format: 'A4',
-  //   orientation: 'portrait',
-  //   border: {
-  //     top: '0.5in',
-  //     right: '0.5in',
-  //     bottom: '0.5in',
-  //     left: '0.5in'
-  //   }
-  // };
+  const options = {
+    format: 'A4',
+    orientation: 'portrait',
+    border: {
+      top: '0.5in',
+      right: '0.5in',
+      bottom: '0.5in',
+      left: '0.5in'
+    }
+  };
   
-  // pdf.create(pdfTemplate(req.body), options).toFile('invoice.pdf', (err) => {
-  //   if (err) {
-  //     console.error(err);
-  //     res.status(500).send('Error generating PDF');
-  //   } else {
-  //     res.send('PDF generated successfully!');
-  //   }
-  // });
-  let file = pdfTemplate(req.body);
-  fs.writeFileSync('./document/invoice.html', file, 'binary');
-  (async () => {
-    const browser = await puppeteer.launch({  headless:"new",  executablePath: '/usr/bin/chromium-browser'});
-    const page = await browser.newPage();
-    let pathStatic = req.protocol + '://' + req.get('host') + "/static/invoice.html";
-    await page.goto(pathStatic, {waitUntil: 'networkidle2'});
-    await page.pdf({path: 'invoice.pdf', format: 'A4'});
-  
-    await browser.close();
-  })().then(()=>{
-    res.send('PDF generated successfully!');
+  pdf.create(pdfTemplate(req.body), options).toFile('invoice.pdf', (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error generating PDF');
+    } else {
+      res.send('PDF generated successfully!');
+    }
   });
-
 };
+
+
+
+
 
 exports.fetchPdf = (req, res) => {
   res.sendFile(path.join(__dirname, "invoice.pdf"));
