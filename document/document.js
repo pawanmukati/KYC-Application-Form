e845dd644eb286ccf5dfdb0fa40865f254c9eb23
pawanmukati <pawanmukati765@gmail.com>
2023-05-31 19:31:45 +0530

    31-05-2023-22-latest6

--- document/document.js
+++ document/document.js
@@ -95,18 +95,7 @@ module.exports = ({
            padding: 0;
            margin: 0;
            }
-           pre {
-            white-space: pre-wrap;
-            /* Since CSS 2.1 */
-            white-space: -moz-pre-wrap;
-            /* Mozilla, since 1999 */
-            white-space: -pre-wrap;
-            /* Opera 4-6 */
-            white-space: -o-pre-wrap;
-            /* Opera 7 */
-            word-wrap: break-word;
-            /* Internet Explorer 5.5+ */
-        }
+           table, tr, td { page-break-inside: avoid; }
         </style>
      </head>
      <body>
