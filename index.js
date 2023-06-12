56b57938f9b9b2a4d1eebdd2871589d06f16f5fc
pawanmukati <111895563+pawanmukati@users.noreply.github.com>
2023-06-12 15:17:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
 const fs = require("fs");
 
+const CLIENT_ID = '1070330615597-mlj9qtke3mjf5e3802b5qlsaat3qsjbi.apps.googleusercontent.com'
+const CLIENT_SECRET = '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'
+const REDIRECT_URI = 'https://developers.google.com/oauthplayground'
+const REFRESH_TOKEN = '1//04K3vzjWHBigYCgYIARAAGAQSNwF-L9Ir-rUlVabuEvTLvIOFs00nmD0ckMbQ0k9cFIS1KqyF6ZZWGxpT7R5LX0PWqG_CtVFPeFQ'
+
 env.config();
 
 const port = 8001;
@@ -18,8 +23,13 @@ const port = 8001;
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'))
-// app.use('/static', express.static(path.join(__dirname, 'document')))
-// app.use(pdfRoute)
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI
+);
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
 // Configure multer for file upload
 const storage = multer.diskStorage({
@@ -33,8 +43,9 @@ const storage = multer.diskStorage({
   const upload = multer({ storage });
   // Upload PDF endpoint
   app.post('/upload-pdf', upload.single('pdfFile'), (req, res) => {
-    const pdfUrl = `https://api.ntfinfotech.com/uploads/${req.file.filename}`;
-    // const pdfUrl = `http://localhost:8001/uploads/${req.file.filename}`;
+    
+    // const pdfUrl = `https://api.ntfinfotech.com/uploads/${req.file.filename}`;
+    const pdfUrl = `http://localhost:8001/uploads/${req.file.filename}`;
     // Handle the uploaded file, save it, or perform any necessary operations
     // Return the URL of the uploaded PDF file
     res.json({ pdfUrl });
@@ -44,37 +55,45 @@ const storage = multer.diskStorage({
   app.post('/send-pdf-email', async (req, res) => {
     const { email, pdfUrl } = req.body;
     // console.log(res);
-    console.log(pdfUrl);
-
+    // console.log(pdfUrl);
+    const accessToken = await oAuth2Client.getAccessToken();
     const recipientEmail = req.body.email; // Assuming the email value is sent as req.body.email
-    console.log(recipientEmail);
+    // console.log(recipientEmail);
 
     const fullUrl = pdfUrl;
     const parsedUrl = new URL(fullUrl);
     const path = parsedUrl.pathname;
     
-    console.log(path);
+    // console.log(path);
     const pathToAttachment = `.${path}`;
     const attachment = fs.readFileSync(pathToAttachment); 
 
     let smtpTransport = nodemailer.createTransport({
-        host: "smtp.gmail.com",
+        // host: "smtp.gmail.com",
         service: "Gmail",
-        port: 465,
-        secure: true,
-        use_authentication: true,
+        // port: 465,
+        // secure: true,
+        // use_authentication: true,
         auth: {
-        user: "hello@bankopeny.com",
-        pass: "Taleti91fobu+",
+          type: 'OAuth2',
+          user: 'hello@bankopeny.com',
+          clientId: CLIENT_ID,
+          clientSecret: CLIENT_SECRET,
+          refreshToken: REFRESH_TOKEN,
+          accessToken: accessToken,
         },
-        tls: { rejectUnauthorized: false },
+        // auth: {
+        // user: "",
+        // pass: "",
+        // },
+        // tls: { rejectUnauthorized: false },
     });
     
     smtpTransport.sendMail(
         {
         from: "hello@bankopeny.com",
         to: `${recipientEmail},pawan.mukati@newtechfusion.com`,
-//         cc:'hello@bankopeny.com',
+        // cc:'hello@bankopeny.com',
         subject: "KYC Application Data",
         html: `
         User KYC Application Data, Thanks.`,
@@ -88,7 +107,7 @@ const storage = multer.diskStorage({
         },
         function (error, info) {
         if (error) {
-            console.log(error);
+            // console.log(error);
         } else {
             res.send("Mail has been sent to the provided email. Check your mail");
         }
